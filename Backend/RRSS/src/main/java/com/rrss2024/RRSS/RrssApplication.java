55befa7a84a925637bb7a7cc9c00944a546e625c
Yiğit Alp Demirci <ahmet_1719@outlook.com>
2024-04-22 22:59:58 +0300

    Hi!

--- Backend/RRSS/src/main/java/com/rrss2024/RRSS/RrssApplication.java
+++ Backend/RRSS/src/main/java/com/rrss2024/RRSS/RrssApplication.java
@@ -3,6 +3,7 @@ package com.rrss2024.RRSS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //test commit
+// Hello, World!
 @SpringBootApplication
 public class RrssApplication {
 
