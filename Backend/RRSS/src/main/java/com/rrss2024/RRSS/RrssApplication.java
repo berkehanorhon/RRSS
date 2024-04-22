18b9ba0c29cf2f830aae81229bccdeeebcf8db27
Yiğit Alp Demirci <ahmet_1719@outlook.com>
2024-04-22 23:24:31 +0300

    test  123

--- Backend/RRSS/src/main/java/com/rrss2024/RRSS/RrssApplication.java
+++ Backend/RRSS/src/main/java/com/rrss2024/RRSS/RrssApplication.java
@@ -2,7 +2,7 @@ package com.rrss2024.RRSS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//test commit
+//test commit Again!
 @SpringBootApplication
 public class RrssApplication {
 
