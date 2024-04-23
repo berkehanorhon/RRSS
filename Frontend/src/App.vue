3a3b7bf5a74e5ad2dbd0a54978ebca78a81d7c1f
Berkehan <b2200356072@cs.hacettepe.edu.tr>
2024-04-23 21:59:25 +0300

    frontend first commit

---
+++ Frontend/src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link v-if="!isLoggedIn" to="/login">Login</router-link>
+    <router-link v-if="isLoggedIn" to="/login" @click="logout">Logout</router-link> |
+    <router-link to="/register">Register</router-link>
+    <button class="user-icon-button" @click="navigateUser">
+      <img src="https://img.icons8.com/ios-filled/50/000000/user-male-circle.png" alt="User" />
+    </button>
+  </nav>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoggedIn: !!localStorage.getItem('token')
+    };
+  },
+  methods: {
+    navigateUser() {
+      if (this.isLoggedIn) {
+        this.$router.push('/');
+      } else {
+        this.$router.push('/login');
+      }
+    },
+    logout(event) {
+      event.preventDefault();
+      localStorage.removeItem('token');
+      window.location.href = '/';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+  border-bottom: 1px solid black; /* Siyah çizgi ekleniyor */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+body {
+    background-color: #f0f0f0; /* Açık gri */
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.user-icon-button {
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+.user-icon-button img {
+  height: 32px; /* Simgenin boyutunu ayarlayabilirsiniz */
+}
+</style>
---
+++ Frontend/src/views/RegisterView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <RegisterPage />
+    </div>
+  </template>
+  
+  <script>
+  import RegisterPage from '../components/RegisterPage.vue';
+  
+  export default {
+    components: {
+        RegisterPage
+    }
+  }
+  </script>
